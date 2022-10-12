b0bfe60d1ac8c2e2762d29cd363b233af8ec7c5c
João Pedro Ruas <83050781+hagsir@users.noreply.github.com>
2022-10-12 15:53:49 -0300

    Update Baralho.c

--- Nível Sênior/Baralho.c
+++ Nível Sênior/Baralho.c
@@ -1,65 +1,42 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(void){
-char cartas[156], erro[20];
-int C = 13, E = 13, U = 13, P = 13, i=0, j=0;
+int main(void)
+{
+    char cartas[156], letrasNaipes[4] = {"CEUP"};
+    int naipes[4] = {13, 13, 13, 13}, i, j, k;
+
     scanf("%s", cartas);
-    
-   if(strlen(cartas)>2){
-    for(i=0; i<strlen(cartas); i+=3){
-        for(j=0; j<strlen(cartas); j+=3){
-            if(cartas[i] == cartas[j] && cartas[i+1] == cartas[j+1] && cartas[i+2] == cartas[j+2] && i!=j){
-                if(cartas[i+2] == 'C'){
-                    C-=14;
+
+    if (strlen(cartas) > 2){
+        for (i = 0; i < strlen(cartas); i += 3){
+            for (j = 0; j < strlen(cartas); j += 3){
+                if (cartas[i] == cartas[j] && cartas[i + 1] == cartas[j + 1] && cartas[i + 2] == cartas[j + 2] && i != j){
+                    for (k = 0; k < 4; k++){
+                        if (cartas[i + 2] == letrasNaipes[k]){
+                            naipes[k] = -14;
+                        }
+                    }
                 }
-                if(cartas[i+2] == 'E'){
-                    E-=14;
-                }
-                if(cartas[i+2] == 'U'){
-                    U-=14;
-                }
-                if(cartas[i+2] == 'P'){
-                    P-=14;
-                }
-            }    
+            }
         }
-    }
-    
-    
-        for(int i=0;  i<strlen(cartas); i++){
-            if(cartas[i] == 'C'){
-                C--;
-            }
-            if(cartas[i] == 'E'){
-                E--;
-            }
-            if(cartas[i] == 'U'){
-                U--;                
-            }
-            if(cartas[i] == 'P'){
-                P--;
+
+        for (i = 0; i < strlen(cartas); i++){
+            for (int j = 0; j < 4; j++){
+                if (cartas[i] == letrasNaipes[j]){
+                    naipes[j]--;
+                }
             }
         }
     }
 
-    if(C>=0){
-        printf("%d\n", C);
-    }else
-        printf("erro\n");
-    if(E>=0){
-        printf("%d\n", E);
-    }else
-        printf("erro\n");
-    if(U>=0){
-        printf("%d\n", U);
-    }else
-        printf("erro\n");
-    if(P>=0){
-        printf("%d\n", P);
-    }else
-        printf("erro\n");
-
+    for (i = 0; i < 4; i++){
+        if (naipes[i] >= 0){
+            printf("%d\n", naipes[i]);
+        }
+        else{
+            printf("erro\n");
+        }
+    }
     return 0;
-    
 }
