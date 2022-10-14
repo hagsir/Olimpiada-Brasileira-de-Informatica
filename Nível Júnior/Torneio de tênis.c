49591f503b146e54303901cbf4afff4e67e2b0e0
João Pedro Ruas <83050781+hagsir@users.noreply.github.com>
2022-10-13 21:09:15 -0300

    Update Torneio de tênis.c

--- Nível Júnior/Torneio de tênis.c
+++ Nível Júnior/Torneio de tênis.c
@@ -1,26 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-char resultado[6];
-int vitorias=0;
+    char resultado[6];
+    int vitorias = 0;
 
-    for(int i=0; i<6; i++){
+    for (int i = 0; i < 6; i++){
         fgets(resultado, 6, stdin);
-        if(resultado[0] == 'V'){
+        if (resultado[0] == 'V'){
             vitorias++;
         }
     }
 
-    if(vitorias>4){
+    if (vitorias > 4){
         printf("1");
-    }else if(vitorias>2){
-        printf("2");
-    }else if(vitorias>0){
-        printf("3");
-    }else{
-        printf("-1");
-    }
+        }else if (vitorias > 2){
+            printf("2");
+            }else if (vitorias > 0){
+                printf("3");
+                }else{
+                    printf("-1");
+                }
 
-
-return 0;
+    return 0;
 }
